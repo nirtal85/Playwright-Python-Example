caf9c75b2a66eec91466e63f878a02766c43361c
Nir Tal <nirt236@gmail.com>
2021-11-05 00:30:48 +0200

    updated

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,4 @@
+import allure
 import pytest
 
 from utils.config_parser import ConfigParserIni
@@ -7,3 +8,25 @@ from utils.config_parser import ConfigParserIni
 # instantiates ini file parses object
 def prep_properties():
     return ConfigParserIni("props.ini")
+
+
+@pytest.fixture(autouse=True)
+# Performs setup and tear down
+def attach_playwright_results(page, prep_properties, request):
+    yield
+    if request.node.rep_call.failed:
+        allure.attach(page.screenshot(full_page=True), name="Screen shot on failure",
+                      attachment_type=allure.attachment_type.PNG)
+
+
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    # execute all other hooks to obtain the report object
+
+    outcome = yield
+    rep = outcome.get_result()
+
+    # set a report attribute for each phase of a call, which can
+    # be "setup", "call", "teardown"
+
+    setattr(item, "rep_" + rep.when, rep)
