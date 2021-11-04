75f1e4c9dbea07e4b8f3f8dbd69bd49acf241c04
Nir Tal <nirt236@gmail.com>
2021-11-05 01:22:57 +0200

    updated

--- tests/test_base.py
+++ tests/test_base.py
@@ -10,4 +10,4 @@ def test_invalid_login(page):
     page.type("[data-test=password]", "secret_sauce1")
     page.click("[data-test=login-button]")
     assert (page.text_content(
-        "data-test=error") == "Epic sadface: Username and passdfgfdgfdword do not match any user in this service")
+        "data-test=error") == "Epic sadface: Username and password do not match any user in this service")
