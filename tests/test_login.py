fe89715b574403fb0f8a6ade0060e54024a87b47
Nir Tal <nirt236@gmail.com>
2021-11-07 15:07:59 +0200

    updated

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,26 +1,30 @@
+import pytest
+
+from pages.login_page import LoginPage
+
 ex_url = "https://www.saucedemo.com/inventory.html"
 ex_error_message = "Epic sadface: Username and password do not match any user in this service"
 ex_locked_out_user_message = "Epic sadface: Sorry, this user has been locked out."
 
 
-class LoginTest:
+class TestLogin:
 
-    # @pytest.fixture(autouse=True)
-    # # Instantiates Page Objects
-    # def setup(self, elements):
-    #     self.login_page = LoginPage(elements)
+    @pytest.fixture(autouse=True)
+    # Instantiates Page Objects
+    def setup(self, elements):
+        self.login_page = LoginPage(elements)
 
-    def test_valid_login(self):
+    def test_valid_login(self, elements, page):
         self.login_page.login("standard_user", "secret_sauce")
-        # assert page.url == ex_url, f"Expected url to be {ex_url} after a valid login, but got {page.url} instead"
+        assert page.url == ex_url, f"Expected url to be {ex_url} after a valid login, but got {page.url} instead"
 
-    # def test_invalid_login(self, page, elements):
-    #     self.login_page.login("standard_user", "secret_sauce1")
-    #     assert self.login_page.get_error_message_text() == ex_error_message, \
-    #         f"Expected invalid login error message to be {ex_error_message}, but got {LoginPage(elements).get_error_message_text()} instead "
-    #
-    # def test_locked_out_user(self, page, elements):
-    #     self.login_page.login("locked_out_user", "secret_sauce")
-    #     assert self.login_page.get_error_message_text() == ex_locked_out_user_message, \
-    #         f"Expected locked out user message to be {ex_locked_out_user_message}, " \
-    #         f"but got {LoginPage(elements).get_error_message_text()} instead"
+    def test_invalid_login(self, page, elements):
+        self.login_page.login("standard_user", "secret_sauce1")
+        assert self.login_page.get_error_message_text() == ex_error_message, \
+            f"Expected invalid login error message to be {ex_error_message}, but got {self.login_page.get_error_message_text()} instead "
+
+    def test_locked_out_user(self, page, elements):
+        self.login_page.login("locked_out_user", "secret_sauce")
+        assert self.login_page.get_error_message_text() == ex_locked_out_user_message, \
+            f"Expected locked out user message to be {ex_locked_out_user_message}, " \
+            f"but got {self.login_page.get_error_message_text()} instead"
