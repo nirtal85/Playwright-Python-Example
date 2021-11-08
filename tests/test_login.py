ab34a402ac94388561bc3dd9c011ad4a02b2e3c5
unknown <eshorrosh@gmail.com>
2021-11-09 00:19:11 +0200

    Add allure annotations

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,5 +1,5 @@
 import pytest
-
+import allure
 from pages.login_page import LoginPage
 
 ex_url = "https://www.saucedemo.com/inventory.html"
@@ -14,16 +14,23 @@ class TestLogin:
     def setup(self, elements):
         self.login_page = LoginPage(elements)
 
+    @allure.title("Tests a valid login")
+    @allure.description("Assert that a valid login with valid username and password redirects user to expected page")
     def test_valid_login(self, elements, page):
         self.login_page.login("standard_user", "secret_sauce")
         assert page.url == ex_url, f"Expected url to be {ex_url} after a valid login, but got {page.url} instead"
 
+    @allure.title("Tests an invalid login")
+    @allure.description(
+        "Assert that an invalid login with invalid username and password displays proper error message")
     def test_invalid_login(self, page, elements):
         self.login_page.login("standard_user", "secret_sauce1")
         assert self.login_page.get_error_message_text() == ex_error_message, \
             f"Expected invalid login error message to be {ex_error_message}, " \
             f"but got {self.login_page.get_error_message_text()} instead "
 
+    @allure.title("Tests login for locked out user")
+    @allure.description("Assert that a locked out user gets the proper error message")
     def test_locked_out_user(self, page, elements):
         self.login_page.login("locked_out_user", "secret_sauce")
         assert self.login_page.get_error_message_text() == ex_locked_out_user_message, \
