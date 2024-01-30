62909e1e0061a89feb722bb42c5e40a0b76df357
nirtal85 <nirt236@gmail.com>
2024-01-30 17:18:37 +0200

    updated

--- tests/test_login.py
+++ tests/test_login.py
@@ -3,12 +3,6 @@ from playwright.sync_api import Page, expect
 
 from pages.login_page import LoginPage
 
-ex_url = "https://www.saucedemo.com/inventory.html"
-ex_error_message = (
-    "Epic sadface: Username and password do not match any user in this service"
-)
-ex_locked_out_user_message = "Epic sadface: Sorry, this user has been locked out."
-
 
 class TestLogin:
     @pytest.fixture(autouse=True)
@@ -16,15 +10,16 @@ class TestLogin:
     def setup(self, page: Page):
         self.login_page = LoginPage(page)
 
-    def test_valid_login(self, page: Page):
+    def test_valid_login(self, base_url, page: Page):
         self.login_page.login("standard_user", "secret_sauce")
-        expect(page).to_have_url(ex_url)
+        expect(page).to_have_url(f"{base_url}inventory.html")
 
     def test_invalid_login(self, page: Page):
         self.login_page.login("standard_user", "secret_sauce1")
-        expect(self.login_page.error_message).to_have_text(ex_error_message)
+        expect(self.login_page.error_message).to_have_text(
+            "Epic sadface: Username and password do not match any user in this service")
 
     @pytest.mark.devRun
     def test_locked_out_user(self, page: Page):
         self.login_page.login("locked_out_user", "secret_sauce")
-        expect(self.login_page.error_message).to_have_text(ex_locked_out_user_message)
+        expect(self.login_page.error_message).to_have_text("Epic sadface: Sorry, this user has been locked out.")
