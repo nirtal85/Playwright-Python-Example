5eebfe2780a42c58e464e2ca9dbff07120d2c09f
nirtal85 <nirt236@gmail.com>
2024-02-19 09:09:47 +0200

    updated

--- tests/login_test.py
+++ tests/login_test.py
@@ -10,19 +10,29 @@ class TestLogin:
     def setup(self, page: Page):
         self.login_page = LoginPage(page)
 
+    @pytest.mark.devRun
     def test_valid_login(self, base_url, page: Page):
         self.login_page.login("standard_user", "secret_sauce")
         expect(page).to_have_url(f"{base_url}inventory.html")
 
-    def test_invalid_login(self, page: Page):
-        self.login_page.login("standard_user", "secret_sauce1")
-        expect(self.login_page.error_message).to_have_text(
-            "Epic sadface: Username and password do not match any user in this service"
-        )
-
-    @pytest.mark.devRun
-    def test_locked_out_user(self, page: Page):
-        self.login_page.login("locked_out_user", "secret_sauce")
-        expect(self.login_page.error_message).to_have_text(
-            "Epic sadface: Sorry, this user has been locked out."
-        )
+    @pytest.mark.parametrize(
+        "username, password, expected_error",
+        [
+            (
+                "standard_user",
+                "secret_sauce1",
+                "Epic sadface: Username and password do not match any user in this service",
+            ),
+            (
+                "locked_out_user",
+                "secret_sauce",
+                "Epic sadface: Sorry, this user has been locked out.",
+            ),
+        ],
+        ids=["invalid_password", "locked_user"],
+    )
+    def test_login_error(
+        self, page: Page, username: str, password: str, expected_error: str
+    ):
+        self.login_page.login(username, password)
+        expect(self.login_page.error_message).to_have_text(expected_error)
